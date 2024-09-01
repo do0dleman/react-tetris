0e3027640ddebb352ae925511f64c461c1899e05
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-09-01 15:11:21 +0300

    changed cell border color

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,16 +1,22 @@
-import { ShapeType } from "../../models/shapes"
-import "./cell.css"
+import { ShapeType } from "../../models/shapes";
+import "./cell.css";
 
 type CellProps = {
-    ShapeType: ShapeType | "ghost"
-    className?: string
-}
+  ShapeType: ShapeType | "ghost";
+  className?: string;
+};
 
 function Cell({ ShapeType, className }: CellProps) {
-    return (
-        <div className={`w-[30px] md:w-10 aspect-square border border-slate-400 p-0.5 ${ShapeType} ${className}`} >
-            {ShapeType != undefined ? <div className="w-full h-full border-4 border-black border-opacity-20" /> : <></>}
-        </div>
-    )
+  return (
+    <div
+      className={`w-[30px] md:w-10 aspect-square border border-slate-700 p-0.5 ${ShapeType} ${className}`}
+    >
+      {ShapeType != undefined ? (
+        <div className="w-full h-full border-4 border-black border-opacity-20" />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
-export default Cell
+export default Cell;
